112460060b7eb36666297b8a910f3fd888fe7383
Gadi Cohen <dragon@wastelands.net>
2024-12-20 08:48:14 +0200

    feat(serwist): enable/migrate app upgrade logic from wb to serwist

--- src/app/clientProviders.tsx
+++ src/app/clientProviders.tsx
@@ -3,18 +3,17 @@ import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-// import workboxStuff from "@/workboxStuff";
+import serwistStuff from "@/serwistStuff";
 
 export default function ClientProviders({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  /*
   React.useEffect(() => {
-    workboxStuff();
+    // serwistStuff() setups up and returns a cleanup function
+    return serwistStuff();
   }, []);
-  */
 
   return (
     <SessionProvider>
--- src/serwistStuff.tsx
+++ src/serwistStuff.tsx
@@ -2,39 +2,27 @@
 const t = String.raw.bind(String);
 import asyncConfirm from "./asyncConfirm";
 
-if (typeof navigator === "object" && "serviceWorker" in navigator) {
-  navigator.serviceWorker.ready.then(function (registration) {
-    const updateInterval = 60_000;
-    console.log(`Will check for updates ever ${updateInterval / 1000} seconds`);
-    setInterval(() => {
-      console.log("Checking for updates...");
-      registration.update();
-    }, updateInterval);
-  });
-}
-
-export default function workboxStuff() {
+export default function serwistStuff() {
   if (
     typeof window !== "undefined" &&
     "serviceWorker" in navigator &&
-    // @ts-expect-error: blah
-    window.workbox !== undefined
+    window.serwist !== undefined
   ) {
-    // @ts-expect-error: blah
-    const wb = window.workbox;
+    const sw = window.serwist;
+
     // add event listeners to handle any of PWA lifecycle event
     // https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-window.Workbox#events
-    wb.addEventListener("installed", (event) => {
+    sw.addEventListener("installed", (event) => {
       console.log(`Event ${event.type} is triggered.`);
       console.log(event);
     });
 
-    wb.addEventListener("controlling", (event) => {
+    sw.addEventListener("controlling", (event) => {
       console.log(`Event ${event.type} is triggered.`);
       console.log(event);
     });
 
-    wb.addEventListener("activated", (event) => {
+    sw.addEventListener("activated", (event) => {
       console.log(`Event ${event.type} is triggered.`);
       console.log(event);
     });
@@ -51,12 +39,12 @@ export default function workboxStuff() {
           t`A newer version of this web app is available, reload to update?`
         )
       ) {
-        wb.addEventListener("controlling", (_event) => {
+        sw.addEventListener("controlling", (_event) => {
           window.location.reload();
         });
 
         // Send a message to the waiting service worker, instructing it to activate.
-        wb.messageSkipWaiting();
+        sw.messageSkipWaiting();
       } else {
         console.log(
           "User rejected to reload the web app, keep using old version. New version will be automatically load when user open the app next time."
@@ -64,31 +52,49 @@ export default function workboxStuff() {
       }
     };
 
-    wb.addEventListener("waiting", promptNewVersionAvailable);
+    sw.addEventListener("waiting", promptNewVersionAvailable);
 
-    // ISSUE - this is not working as expected, why?
+    // ISSUE - this is not working as expected, why?  (for workbox, didn't test serwist)
     // I could only make message event listenser work when I manually add this listenser into sw.js file
-    wb.addEventListener("message", (event) => {
+    sw.addEventListener("message", (event) => {
       console.log(`Event ${event.type} is triggered.`);
       console.log(event);
     });
 
-    /*
-    wb.addEventListener('redundant', event => {
-      console.log(`Event ${event.type} is triggered.`)
-      console.log(event)
-    })
-    wb.addEventListener('externalinstalled', event => {
-      console.log(`Event ${event.type} is triggered.`)
-      console.log(event)
-    })
-    wb.addEventListener('externalactivated', event => {
-      console.log(`Event ${event.type} is triggered.`)
-      console.log(event)
-    })
-    */
+    sw.addEventListener("redundant", (event) => {
+      console.log(`Event ${event.type} is triggered.`);
+      console.log(event);
+    });
 
     // never forget to call register as auto register is turned off in next.config.js
-    wb.register();
+    sw.register();
+
+    let interval;
+    if (typeof navigator === "object" && "serviceWorker" in navigator) {
+      navigator.serviceWorker.ready.then(function (registration) {
+        const updateInterval = 60_000;
+        console.log(
+          `[PWA] Will check for updates ever ${updateInterval / 1000} seconds`
+        );
+        interval = setInterval(() => {
+          console.log("[PWA] Checking for updates...");
+          registration.update();
+        }, updateInterval);
+      });
+    }
+
+    window.addEventListener("beforeinstallprompt", (event) => {
+      // https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/How_to/Trigger_install_prompt
+      console.log("beforeinstallprompt - TODO");
+    });
+
+    return function cleanup() {
+      clearInterval(interval);
+      if (typeof navigator === "object" && "serviceWorker" in navigator) {
+        navigator.serviceWorker.ready.then(function (registration) {
+          registration.unregister();
+        });
+      }
+    };
   }
 }
